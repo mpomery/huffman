9ec5b0aa0091b7d57de27b70602aa81e4564ea5a
mpomery <bob_george33@hotmail.com>
2013-12-04 21:35:00 +0800

    updated compile script
    Moved compiled byte code and testing script to new directory.

--- ~compile.bat
+++ ~compile.bat
@@ -3,6 +3,7 @@
 cls
 echo Compiling
 javac -cp CITS2200.jar Compress.java Huffman.java HuffmanTree.java BitByteConverter.java WriteBuffer.java
+move /Y *.class compiled/
 echo.
 echo Compiled
 pause
